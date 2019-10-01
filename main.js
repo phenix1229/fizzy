50b8cdc8141056ef7caef2d5221b26c8710b8ea6
phenix1229 <paul.garay@codeimmersives.com>
2019-10-01 15:12:48 -0400

    solved one way

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function fizzy(num) {
-
+  if (num % 5 === 0 && !(num % 3 === 0)){
+    return 'Buzz'
+  } 
+  else if (num % 3 === 0 && !(num % 5 === 0)){
+    return 'Fizz'
+  } 
+        else if (num % 3 === 0 && num % 5 === 0){
+          return 'FizzBuzz'
+        }
+    else {
+      return num
+    }
 }
+
+console.log(fizzy(15))
 
 module.exports = {
   fizzy,
